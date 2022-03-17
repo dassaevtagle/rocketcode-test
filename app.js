33e252e2cd4fc1030cf7b64f1b9a95db1e87f4b4
dassaevtagle <dassaev.tagle@gmail.com>
2022-03-17 05:28:53 -0600

    Add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config()
@@ -8,6 +9,11 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
