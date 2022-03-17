b7ca96c82541f0f15a46969b24bbcff82c6b693c
dassaevtagle <dassaev.tagle@gmail.com>
2022-03-17 13:05:53 -0600

    Production redirection on server

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+if(['production'].includes(process.env.NODE_ENV)){
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.use('/api/users', usersRouter);
 
 module.exports = app;
